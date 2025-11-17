0b5c679e2a1c483adf80b3b0505746e14f349102
gero1nimo <mehmetayzit351@gmail.com>
2025-11-17 15:14:55 +0300

    Essential Revision

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests
+import requests, json
 
 
 
@@ -12,7 +12,8 @@ def connection(method: str, url: str):
     session = requests.Session()
     try:
         request = session.request(method=method, url=url)
-        return request
+        return request if request.status_code == 200 else None
+    
     except requests.exceptions.RequestException as err:
         print(err)
     
@@ -22,21 +23,35 @@ def fetch_data(owner: str):
     url = f"https://api.github.com/users/{owner}/events"
     try:
         request = connection("Get", url)
-    
+        return request.json() if request != None else None
+        
     except requests.exceptions.RequestException as err:
         print(err.args)
-        return None
     
-    return request.json() if request.status_code == 200 else None
+    return None
+    
 
 def format_data(data):
+    activity_types = {}
     for d in data:
-        print(d)
+        activity_types[d['type']] = activity_types.get(d['type'], 0) +1
+        print(activity_types)
 
 # format_data(fetch_data("gero1nimo"))
     
   
-username = str(input("Enter the username: "))        
+# username = str(input("Enter the username: "))        
+username = "gero1nimo"        
 
-print(fetch_data(username))
+data = fetch_data(username)
+if data != None:
+    print(len(fetch_data(username))) 
+    print(type(fetch_data(username)))
+    format_data(data)
 
+
+
+# with open("response.json", "a") as file:
+#     json.dump(fetch_data(username),file, indent=4)
+    
+# file.close()
